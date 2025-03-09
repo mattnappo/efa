5ae3cb82a8f991075d6c7a2743a976a9b976ddba
Matt Nappo <mattnappo@gmail.com>
2025-03-09 18:13:37 -0400

    Switching to functional style

--- efa-core/src/solver/toposort.rs
+++ efa-core/src/solver/toposort.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 
@@ -31,50 +32,53 @@ where
     pub fn new(graph: &'a Graph<T>) -> Result<()> {
         let sorted = vec![];
 
-        let marks = graph
+        let mut marks = graph
             .keys()
             .map(|k| (k, Mark::Unmarked))
             .collect::<HashMap<_, _>>();
 
-        let mut topo = Self {
-            sorted,
-            marks,
-            graph,
-        };
-
-        while let Some(node) = topo.unmarked() {
-            topo.marks.insert(node, Mark::Temp);
-            topo.visit(node)?;
+        while let Some(node) = Self::unmarked(&marks) {
+            marks.insert(node, Mark::Temp);
+            (marks, sorted) = Self::visit(node, &marks, &sorted, graph)?;
         }
 
         Ok(())
     }
 
-    fn unmarked(&self) -> Option<&T> {
-        self.marks.iter().find_map(|(node, mark)| match mark {
+    fn unmarked(marks: &'a HashMap<&'a T, Mark>) -> Option<&T> {
+        marks.iter().find_map(|(node, mark)| match mark {
             Mark::Unmarked | Mark::Temp => Some(*node),
             Mark::Final => None,
         })
     }
 
-    fn visit(&mut self, node: &'a T) -> Result<()> {
-        if self.marks.get(node).unwrap() == &Mark::Final {
-            return Ok(());
+    fn visit(
+        node: &'a T,
+        marks: &HashMap<&'a T, Mark>,
+        sorted: &Vec<&'a T>,
+        graph: &'a Graph<T>,
+    ) -> Result<(HashMap<&'a T, Mark>, Vec<&'a T>)> {
+        if marks.get(node).unwrap() == &Mark::Final {
+            return Ok((HashMap::new(), sorted.clone()));
         }
 
-        if self.marks.get(node).unwrap() == &Mark::Temp {
+        if marks.get(node).unwrap() == &Mark::Temp {
             bail!("toposort: error: graph has cycle");
         }
 
-        self.marks.insert(node, Mark::Temp);
+        let mut marks = marks.clone();
 
-        for in_node in self.graph.get(node).unwrap() {
-            self.visit(in_node)?;
+        marks.insert(node, Mark::Temp);
+
+        for in_node in graph.get(node).unwrap() {
+            Self::visit(in_node, &marks, sorted, graph)?;
         }
 
-        self.marks.insert(node, Mark::Final);
-        self.sorted.insert(0, node);
-        Ok(())
+        marks.insert(node, Mark::Final);
+        let mut new_sorted = sorted.clone();
+
+        new_sorted.insert(0, node);
+        Ok((marks, new_sorted))
     }
 }
 
