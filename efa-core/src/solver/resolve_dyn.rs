821e36db540fdc82860257b4936dee61927b6d78
Matt Nappo <mattnappo@gmail.com>
2025-03-09 21:18:34 -0400

    Writing dynamic call resolver

--- efa-core/src/solver/resolve_dyn.rs
+++ efa-core/src/solver/resolve_dyn.rs
@@ -6,11 +6,14 @@ use crate::asm::parser::Parse;
 use crate::bytecode::Instr;
 use crate::vm::CodeObject;
 
+use super::toposort::toposort;
+
 #[derive(Debug)]
 struct DynCallResolver {
     objs: HashMap<String, CodeObject>,
-
     deps: HashMap<String, HashSet<String>>,
+
+    hash_order: Vec<String>,
 }
 
 impl DynCallResolver {
@@ -19,13 +22,35 @@ impl DynCallResolver {
             .into_iter()
             .map(|p| (p.func_name, p.code_obj))
             .collect();
+
         let mut s = Self {
             objs,
             deps: HashMap::new(),
+            hash_order: vec![],
         };
 
         s.deps = s.solve()?;
+        s.hash_order = toposort(&s.deps)?;
         Ok(s)
+    }
+
+    /// Compute the hashes of the code objects, replacing `LoadDyn` instructions with
+    /// `LoadHash` when possible. Takes ownership since the modified code objects are
+    /// returned back.
+    pub fn resolve_dyn_calls(self) -> Result<HashMap<String, CodeObject>> {
+        /*
+           already_hashed = Map<Name, Hash>
+           for name in hash_order.rev()
+               dyns = hash[name].code.filter(LoadDyn)
+               for dyn in dyns:
+                   if dyn.name in already_hashed:
+                       dyn.name = already_hashed[dyn.name]
+               hash = objs[name].hash()
+               already_hashed[name] = hash
+               (name, new_obj)
+           collect into map
+        */
+        todo!()
     }
 
     fn solve(&self) -> Result<HashMap<String, HashSet<String>>> {
